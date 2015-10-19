fa7d0a823553e8d8f1861fdb54baf16369240843
speami02 <speami02@luther.edu>
2015-10-19 13:06:32 -0500

    Update dynamic_todo.js

--- dynamic_todo.js
+++ dynamic_todo.js
@@ -1,1 +1,26 @@
+addTask = function() {
+	var myLi, liText, myOl, myCheckbox;
 
+	myLi = document.createElement('li');
+
+	liText = document.createTextNode(document.querySelector("#newItem").value);
+
+	myOl = document.querySelector("#toDoList");
+
+	myCheckbox = document.createElement('input');
+	myCheckbox.type = "checkbox";
+	myCheckbox.onclick = isDone;
+
+	myLi.className = document.querySelector('#priority').value;
+
+	myLi.appendChild(myCheckbox);
+	myLi.appendChild(liText);
+	myOl.appendChild(myLi);
+}
+isDone = function() {
+	if (this.checked) {
+		this.parentNode.classList.add('done');
+	} else {
+		this.parentNode.classList.remove('done');
+	}
+}
